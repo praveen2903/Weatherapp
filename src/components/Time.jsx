44cafbebdcc00a44cb03cebc2353d1d6a74ac63a
praveen2903 <vvit20bq1a05l0@gmail.com>
2023-09-29 22:29:52 +0530

    current time has been added

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-export default function Time({weather, timecolor}) {
-    const dateTime = new Date(weather.dt * 1000);
-    const toUtc = dateTime.getTime() + dateTime.getTimezoneOffset() * 60000;
-    const currentLocalTime = toUtc + 1000 * weather.timezone;
-    const selectedDate = new Date(currentLocalTime);
-    const date = selectedDate.toLocaleString("en-GB", {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    });
-    const time = selectedDate.toLocaleString("en-GB", {
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-    });
+export default function Time({ weather, timecolor }) {
+  const [mytime, setMyTime] = useState('');
+
+  useEffect(() => {
+    const updateClock = () => {
+      const selectedDate = new Date(Date.now() + weather.timezone * 1000 - 5.5 * 60 * 60 * 1000);
+
+      const time = selectedDate.toLocaleString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+      });
+
+      setMyTime(time);
+      requestAnimationFrame(updateClock);
+    };
+
+    const interval = requestAnimationFrame(updateClock);
+
+    return () => {
+      cancelAnimationFrame(interval);
+    };
+  }, [weather.timezone]);
+
+  const date = new Date().toLocaleString("en-GB", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
     <div>
-        <div className="flex items-center justify-center my-6">
-            <p className={`text-[15px] sm:text-3xl font-semibold ${timecolor}`}>
-              {date} ,{time}{" "}
-            </p>
+      <div className="flex items-center justify-center my-6">
+        <p className={`text-[15px] sm:text-3xl font-semibold ${timecolor}`}>
+          {date} ,{mytime}
+        </p>
       </div>
-
     </div>
-  )
+  );
 }
